489b495b76983ca3c19a7259cdfd2a4f9a6f3403
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2024-11-27 11:40:58 -0300

    Limitando requisições

--- setup/settings.py
+++ setup/settings.py
@@ -134,6 +134,14 @@ REST_FRAMEWORK = {
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20
+    'PAGE_SIZE': 20, 
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '1000/day',
+        'user': '1000/day'
+    }, 
 
 }
