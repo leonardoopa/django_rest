81bf3ae8dbc6bc3ebcbca78e7519bd076f852f4f
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2024-11-27 11:35:29 -0300

    Ordenando as listas de Objetos

--- escola/views.py
+++ escola/views.py
@@ -5,28 +5,28 @@ from rest_framework import serializers
 from django_filters.rest_framework import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
-    queryset = Estudante.objects.all()
+    queryset = Estudante.objects.all().order_by("id")
     serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ['nome']
     searched_fields = ['nome', 'cpf', 'celular']
 
 class CursoViewSet(viewsets.ModelViewSet):
-    queryset = Curso.objects.all()
+    queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     ordering_fields = ['curso']
 
 
 class MatriculaViewSet(viewsets.ModelViewSet): 
-    queryset = Matricula.objects.all()
+    queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer    
     
 
 
 class ListaMatriculas(generics.ListAPIView):
     def get_queryset(self):
-        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk'])
+        queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
@@ -34,7 +34,7 @@ class ListaMatriculasEstudanteView(generics.ListAPIView):
     serializer_class = ListaMatriculasEstudanteSerializer
 
     def get_queryset(self):
-        return Matricula.objects.filter(estudante_id=self.kwargs['pk'])    
+        return Matricula.objects.filter(estudante_id=self.kwargs['pk']).order_by("id")
     class Meta:
         model = Matricula
         fields = ['curso','periodo']
@@ -45,7 +45,7 @@ class ListaMatriculasCursoView(generics.ListAPIView):
     serializer_class = ListaMatriculasCursoSerializer
 
     def get_queryset(self):
-        return Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return Matricula.objects.filter(curso_id=self.kwargs['pk']).order_by("id")
     class Meta:
         model = Matricula
         fields = ['estudante_nome']
