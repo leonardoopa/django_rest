b4499b5ea89c20871cbce76d9691b8600468e682
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2024-11-28 22:14:09 -0300

    fix

--- escola/views.py
+++ escola/views.py
@@ -7,6 +7,25 @@ from rest_framework.throttling import UserRateThrottle
 from escola.thottles import MatriculaAnonRateThrottle
 
 class EstudanteViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de estudantes.
+
+    Campos de ordenação:
+    - nome: permite ordenar os resultados por nome.
+
+    Campos de pesquisa:
+    - nome: permite pesquisar os resultados por nome.
+    - cpf: permite pesquisar os resultados por CPF.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+
+    Classe de Serializer:
+    - EstudanteSerializer: usado para serialização e desserialização de dados.
+    - Se a versão da API for 'v2', usa EstudanteSerializerV2.
+    """
+
     queryset = Estudante.objects.all().order_by("id")
     serializer_class = EstudanteSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -14,6 +33,14 @@ class EstudanteViewSet(viewsets.ModelViewSet):
     searched_fields = ['nome', 'cpf', 'celular']
 
 class CursoViewSet(viewsets.ModelViewSet):
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de cursos.
+
+    Métodos HTTP Permitidos:
+    - GET, POST, PUT, PATCH, DELETE
+    """
+     
     queryset = Curso.objects.all().order_by("id")
     serializer_class = CursoSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
@@ -23,18 +50,44 @@ class MatriculaAnonRateThrottle(MatriculaAnonRateThrottle):
     rate = '5/day'
 
 class MatriculaViewSet(viewsets.ModelViewSet): 
+
+    """
+    Descrição da ViewSet:
+    - Endpoint para CRUD de matrículas.
+
+    Métodos HTTP Permitidos:
+    - GET, POST
+
+    Throttle Classes:
+    - MatriculaAnonRateThrottle: limite de taxa para usuários anônimos.
+    - UserRateThrottle: limite de taxa para usuários autenticados.
+    """
+
     queryset = Matricula.objects.all().order_by("id")
     serializer_class = MatriculaSerializer   
     throttle_classes = [UserRateThrottle, MatriculaAnonRateThrottle] 
 
     
 class ListaMatriculas(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+
     def get_queryset(self):
         queryset = Matricula.objects.filter(estudante_id = self.kwargs['pk']).order_by("id")
         return queryset
     serializer_class = ListaMatriculasEstudanteSerializer
 
 class ListaMatriculasEstudanteView(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Estudante
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
     serializer_class = ListaMatriculasEstudanteSerializer
 
     def get_queryset(self):
@@ -46,6 +99,13 @@ class ListaMatriculasEstudanteView(generics.ListAPIView):
         return obj.get_periodo_display()
 
 class ListaMatriculasCursoView(generics.ListAPIView):
+    """
+    Descrição da View:
+    - Lista Matriculas por id de Curso
+    Parâmetros:
+    - pk (int): O identificador primário do objeto. Deve ser um número inteiro.
+    """
+
     serializer_class = ListaMatriculasCursoSerializer
 
     def get_queryset(self):
