8e1c7c19810bc63514c922d73e4fbabf09411ae2
Leonardo Melo <leonardo.melo@gdstecnologia.com.br>
2024-12-07 16:58:49 -0300

    fix

--- escola/tests/test_models.py
+++ escola/tests/test_models.py
@@ -6,10 +6,18 @@ class ModelEstudanteTestCase(TestCase):
     #     self.fail('Teste falhou :(')
     def setUp(self):
         self.estudante = Estudante.objects.create(
-            nome = 'Joao',
-            email = 'RZL6w@example.com',
+            nome = 'Teste de Modelo',
+            email = 'testedemodelo@gmail.com',
             cpf = '12345678901',
             data_nascimento = '2000-01-01',
             celular = '11999999999'
         )
+
+    def test_verifica_atributos_de_estudante(self):
+        """Teste para verificar os atributos de estudante"""
+        self.assertEqual(self.estudante.nome, 'Teste de Modelo') 
+        self.assertEqual(self.estudante.email, 'testedemodelo@gmail.com')
+        self.assertEqual(self.estudante.cpf, '12345678901')
+        self.assertEqual(self.estudante.data_nascimento, '2000-01-01')
+        self.assertEqual(self.estudante.celular, '11999999999')   
         
